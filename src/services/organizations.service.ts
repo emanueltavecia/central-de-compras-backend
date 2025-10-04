5e374bfe329948fde4a9b1abd01aa8380e84c3ab
Guilherme C. M <gui.cmachado10@gmail.com>
2025-10-04 14:26:25 -0300

    Adição controlle organizations

---
+++ src/services/organizations.service.ts
@@ -0,0 +1,42 @@
+import { OrganizationsRepository } from '@/repository/organizations.repository'
+import { OrganizationSchema } from '@/schemas'
+
+export class OrganizationsService {
+  private repo = new OrganizationsRepository()
+
+  async getOrganizations(filters?: { type?: string; active?: boolean }) {
+    return this.repo.findAll(filters)
+  }
+
+  async getOrganizationById(id: string) {
+    return this.repo.findById(id)
+  }
+
+  async createOrganization(data: OrganizationSchema, createdBy: string) {
+    return this.repo.create(data, createdBy)
+  }
+
+  async updateOrganization(id: string, data: OrganizationSchema) {
+    return this.repo.update(id, data)
+  }
+
+    async deleteOrganization(id: string): Promise<{ deleted: boolean; inactivated: boolean }> {
+    const hasRelations = await this.repo.hasRelations(id)
+
+    if (hasRelations) {
+        await this.repo.updateStatus(id, false)
+        return { deleted: false, inactivated: true }
+    } else {
+        await this.repo.deletePermanent(id)
+        return { deleted: true, inactivated: false }
+    }
+    }
+
+  async updateOrganizationStatus(id: string, active: boolean) {
+    return this.repo.updateStatus(id, active)
+  }
+
+  async getOrganizationUsers(id: string) {
+    return this.repo.findUsers(id)
+  }
+}
